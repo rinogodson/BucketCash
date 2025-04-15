11bc30d0fd50f5a59dace5b731e5e07c372bc684
Rino Godson <rinogodson@gmail.com>
2025-04-15 20:26:01 +0530

    - Added the bucket element with props {perc, money}   perc being the percentage and money being money. :)

--- BucketCash-frontend/src/components/BucketContainer/BucketContainer.jsx
+++ BucketCash-frontend/src/components/BucketContainer/BucketContainer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./BucketContainer.css"
+import { Bucket } from '../Bucket/Bucket'
 
 function BucketContainer() {
   return (
     <div className="container">
-      
+      <Bucket perc={80} money={30}/> 
     </div>
   )
 }
