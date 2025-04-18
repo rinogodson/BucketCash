fce47393d2734d7ec2db655160a67574c8617ee5
Rino Godson <rinogodson@gmail.com>
2025-04-18 20:06:33 +0530

    -- Added Log 'functionality' to the app UI -- Fixed the layout of BucketContainer component to be grid

--- BucketCash-frontend/src/components/BucketContainer/BucketContainer.jsx
+++ BucketCash-frontend/src/components/BucketContainer/BucketContainer.jsx
@@ -5,7 +5,9 @@ import { Bucket } from "../Bucket/Bucket";
 function BucketContainer() {
   return (
     <div className="container">
-      <Bucket perc={80} money={30} color="#C4554D" name="Bucket 1" />
+      <Bucket perc={30} money={"30k"} color="#C4554D" name="Bucket 1" />
+      <Bucket perc={40} money={"40k"} color="#8B67AB" name="Savings" />
+      <Bucket perc={50} money={"50k"} color="#CB782F" name="Pocket" />
     </div>
   );
 }
--- BucketCash-frontend/src/components/Logs.jsx
+++ BucketCash-frontend/src/components/Logs.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import "./Logs.css";
-
+import { motion } from "framer-motion";
 const Logs = () => {
+  const [open, isOpen] = React.useState(false);
+
+  const openHandler = () => {
+    isOpen(!open);
+  };
+
   return (
     <>
-      <div className="logs-container">
-        <h1 className="logs-heading">Logs.</h1>
-        <div className="logs">
-          <LogComponent />
-        </div>
-      </div>
+      <motion.div layout className="logs-container">
+        <h1 className="logs-heading" onClick={openHandler}>
+          Logs.
+        </h1>
+        <div className="logs">{open && <LogComponent />}</div>
+      </motion.div>
     </>
   );
 };
@@ -17,5 +23,27 @@ const Logs = () => {
 export default Logs;
 
 const LogComponent = () => {
-  return <></>;
+  return (
+    <div className="log">
+      1. Transferred 1000 tokens to user123.
+      <br />
+      2. Received 500 tokens from user456.
+      <br />
+      3. Sent 200 tokens to user789.
+      <br />
+      4. Transferred 300 tokens to user101.
+      <br />
+      5. Received 100 tokens from user234.
+      <br />
+      6. Sent 150 tokens to user567.
+      <br />
+      7. Transferred 250 tokens to user890.
+      <br />
+      8. Received 75 tokens from user901.
+      <br />
+      9. Sent 100 tokens to user345.
+      <br />
+      10. Transferred 125 tokens to user678.
+    </div>
+  );
 };
