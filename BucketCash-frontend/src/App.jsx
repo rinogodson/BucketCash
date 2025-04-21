303c3911c7501a247c1c91642dad6cd70db93900
Rino Godson <rinogodson@gmail.com>
2025-04-21 18:39:11 +0530

    fixed ssl protocol error

--- BucketCash-frontend/src/App.jsx
+++ BucketCash-frontend/src/App.jsx
@@ -1,8 +1,5 @@
 import "./App.css";
-import BottomBar from "./components/BottomBar";
-import BucketContainer from "./components/BucketContainer/BucketContainer";
-import Logo from "./components/Logo";
-import Pond from "./components/Pond/Pond";
+
 import { AuthProvider } from "./context/AuthContext";
 import { Home } from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
