f2d9bca337700449e255062d05b510bb7cc186be
rinogodson <rinogodson@gmail.com>
2025-04-25 10:09:27 +0530

    Downgrade Go version for Render compatibility

--- BucketCash-frontend/src/services/api.js
+++ BucketCash-frontend/src/services/api.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
-
-const API_BASE_URL = 'http://localhost:8080/';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
-    'Content-Type': 'application/json'
-  }
-})
+    "Content-Type": "application/json",
+  },
+});
 
 export default api;
